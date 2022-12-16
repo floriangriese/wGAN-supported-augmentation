9442096053b610106075a7f71761a8ba8f171d56
janiskummer <118280386+janiskummer@users.noreply.github.com>
2022-12-16 10:39:48 +0100

    Update radio_galaxy_image_generator_app.py

--- radio_galaxy_image_generator_app.py
+++ radio_galaxy_image_generator_app.py
@@ -4,5 +4,5 @@ import streamlit as st
 if __name__ == "__main__":
     print("app started")
     st.write("hello app on cloud")
-    st.write("Update code")
+    st.write("Update code soon!")
     print("app finished")
